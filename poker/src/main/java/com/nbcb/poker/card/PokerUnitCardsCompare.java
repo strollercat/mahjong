52ca589505c203997425598bd2b2af127dbbc013
ying zheng <nb_zbh@126.com>
2023-07-03 21:41:35 +0800

    Delete poker directory

--- poker/src/main/java/com/nbcb/poker/card/PokerUnitCardsCompare.java
+++
@@ -1,9 +0,0 @@
-package com.nbcb.poker.card;
-
-import com.nbcb.poker.card.PokerUnitCards;
-
-public interface PokerUnitCardsCompare {
-
-	public int compare(PokerUnitCards pucs1, PokerUnitCards pucs2);
-	
-}
--- poker/src/main/java/com/nbcb/poker/threewater/ThreeWaterPokerGameInfo.java
+++
@@ -1,7 +0,0 @@
-package com.nbcb.poker.threewater;
-
-import com.nbcb.core.game.GameInfo;
-
-public class ThreeWaterPokerGameInfo extends GameInfo {
-
-}
