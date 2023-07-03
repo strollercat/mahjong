c9cca6b2ce4233ffdf974a0482840423ecc6ed87
ying zheng <nb_zbh@126.com>
2023-07-03 21:44:43 +0800

    Delete weixinapi directory

--- weixinapi/src/main/java/com/nbcb/weixinapi/transport/Transport.java
+++
@@ -1,8 +0,0 @@
-package com.nbcb.weixinapi.transport;
-
-import com.nbcb.weixinapi.exception.WeixinException;
-
-public interface Transport {
-
-	public Object submit(Object object) throws WeixinException;
-}
